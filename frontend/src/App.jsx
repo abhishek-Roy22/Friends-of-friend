586775679a6be5f6f61e8ce0ed8169c07532097c
abhishek-Roy22 <100830257+abhishek-Roy22@users.noreply.github.com>
2025-01-04 13:10:39 +0530

    Initialize front-end with vite&tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="underline text-3xl font-bold text-rose-800">
+        Hellow World!
+      </h1>
+    </>
+  );
+};
+
+export default App;
